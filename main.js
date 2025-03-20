ca4fad07cea0d9be4615ac0c03a1d688ebec0a08
Kumaran <kumaran.0q@gmail.com>
2025-03-20 11:43:04 +0530

    initialized variables and api key

---
+++ main.js
@@ -0,0 +1,5 @@
+const weatherForm = document.querySelector('.weatherForm');
+const cityInput = document.querySelector('.cityInput');
+const card = document.querySelector('.card');
+const apiKey = 'fb64d7e55cc0f92ddc946d3b8ad81bb9';
+
