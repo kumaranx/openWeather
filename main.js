2e78a5cdc95512e296b0a99222c9327ad840a70f
Kumaran <kumaran.0q@gmail.com>
2025-03-20 11:55:22 +0530

    initialized event listeners and functions.

--- main.js
+++ main.js
@@ -3,3 +3,22 @@ const cityInput = document.querySelector('.cityInput');
 const card = document.querySelector('.card');
 const apiKey = 'fb64d7e55cc0f92ddc946d3b8ad81bb9';
 
+weatherForm.addEventListener('submit', event => {
+
+});
+
+async function getWeatherData(city) {
+
+}
+
+function displayWeatherInfo(data) {
+
+}
+
+function getWeatherEmoji(weatherId) {
+
+}
+
+function displayError(message) {
+
+}
