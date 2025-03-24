ca9204c8f72c6c61b7b64417267ce3677ea20c8c
Kumaran <kumaran.0q@gmail.com>
2025-03-24 09:39:13 +0530

    updated display error on card

--- main.js
+++ main.js
@@ -5,6 +5,16 @@ const apiKey = 'fb64d7e55cc0f92ddc946d3b8ad81bb9';
 
 weatherForm.addEventListener('submit', event => {
 
+  event.preventDefault();
+
+  const city = cityInput.value;
+
+  if (city) {
+
+  }
+  else {
+    displayError('Please enter a city');
+  }
 });
 
 async function getWeatherData(city) {
@@ -21,4 +31,11 @@ function getWeatherEmoji(weatherId) {
 
 function displayError(message) {
 
+  const errorDisplay = document.createElement('p');
+  errorDisplay.textContent = message;
+  errorDisplay.classList.add('errorDisplay');
+
+  card.textContent = '';
+  card.style.display = 'flex';
+  card.appendChild(errorDisplay);
 }
